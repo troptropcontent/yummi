f925941dfef7911d3ec0eb972384cc42f920146a
william castres <william.castres@gmail.com>
2021-03-01 14:58:54 +0100

    update home

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["C'est l'heure de YUMMI ! "],
-    typeSpeed: 30,
+    typeSpeed: 50,
     loop: true
   });
 }
