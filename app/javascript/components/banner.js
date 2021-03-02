847c4ebed3083155e040313ef6c36d8577f9749d
Tom <tomecrepont@gmail.com>
2021-03-02 08:55:01 +0100

    Ok few changes done on the dashboard show

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,14 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["C'est l'heure de YUMMI ! "],
-    typeSpeed: 50,
-    loop: true
-  });
+  const homePage = document.querySelector(".home-page")
+  if (homePage) {    
+    new Typed('#banner-typed-text', {
+      strings: ["C'est l'heure de YUMMI ! "],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
