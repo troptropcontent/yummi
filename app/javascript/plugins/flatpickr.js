d7d02b89e742bfd934224da69bcadece76067974
Robyn Perrin <robynperrin76@gmail.com>
2021-03-02 11:35:27 +0100

    Done with the flatpicker changes

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,11 +1,15 @@
 // app/javascript/plugins/flatpickr.js
 import flatpickr from "flatpickr";
+ import ConfirmDatePlugin from 'flatpickr/dist/plugins/confirmDate/confirmDate';
+  import 'flatpickr/dist/plugins/confirmDate/confirmDate.css'
 
 const initFlatpickr = () => {
   flatpickr(".datepicker", {
     enableTime: true,
     altInput: true,
-    allowInput: true
+    allowInput: true,
+    disableMobile: true,
+    plugins: [new ConfirmDatePlugin({confirmText: 'Done'})]
   });
 }
 
